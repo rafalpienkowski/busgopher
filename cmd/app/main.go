4cfff40002a735473c3db5fdb84a78119f5a81cd
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-09-08 21:02:46 +0200

    Add UI

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"github.com/rivo/tview"
+)
 
 func main() {
-	name := "Raf"
-	message := fmt.Sprintf("Hello world, %v!", name)
-	fmt.Println(message)
+	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
+	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+		panic(err)
+	}
 }
