d16bf53462738c071a1f3c875e9cd6095c8a0e12
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-09-23 22:24:23 +0200

    Produce UI

--- app/main.go
+++ app/main.go
@@ -2,18 +2,57 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 var activeConnection busConnection
 var activeMessage busMessage
 
+func cycleFocus(app *tview.Application, elements []tview.Primitive, reverse bool, logs *tview.TextView) {
+	for i, el := range elements {
+		if !el.HasFocus() {
+			continue
+		}
+
+		if reverse {
+			i = i - 1
+			if i < 0 {
+				i = len(elements) - 1
+			}
+		} else {
+			i = i + 1
+			i = i % len(elements)
+		}
+        fmt.Fprintln(logs, "Focus on: " + strconv.Itoa(i))
+
+		app.SetFocus(elements[i])
+		return
+	}
+}
+
 func main() {
 
 	app := tview.NewApplication()
-
 	logs := tview.NewTextView()
+	connectionsDropDown := tview.NewDropDown()
+	messagesDropDown := tview.NewDropDown()
+
+	inputs := []tview.Primitive{
+		connectionsDropDown,
+		messagesDropDown,
+	}
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyTab {
+			cycleFocus(app, inputs, false, logs)
+		} else if event.Key() == tcell.KeyBacktab {
+			cycleFocus(app, inputs, true, logs)
+		}
+		return event
+	})
 
 	logs.
 		SetDynamicColors(true).
@@ -28,20 +67,39 @@ func main() {
 		return
 	}
 
-    var connectionNames []string
-    for _, conn := range connections {
-        connectionNames = append(connectionNames, conn.Name)
-    }
-    connectionNames = append(connectionNames, "Add..")
+	var connectionNames []string
+	for _, conn := range connections {
+		connectionNames = append(connectionNames, conn.Name)
+	}
+	connectionNames = append(connectionNames, "Add..")
 
-	connectionsDropDown := tview.NewDropDown().
-		SetOptions(connectionNames, func(name string, index int) {
-			fmt.Fprintln(logs, "Selected "+name)
-		}).
-		SetLabel("Select connection: ")
+	connectionsDropDown.SetOptions(connectionNames, func(name string, index int) {
+		fmt.Fprintln(logs, "Selected "+name)
+		activeConnection = connections[index]
+	}).SetLabel("Select connection: ")
+
+	messages, err := loadMessages()
+	if err != nil {
+		fmt.Fprintln(logs, "Can't load messages")
+		fmt.Fprintln(logs, err.Error())
+		return
+	}
+
+	var messageNames []string
+	for _, msg := range messages {
+		messageNames = append(messageNames, msg.Name)
+	}
+	messageNames = append(messageNames, "Add..")
+
+	messagesDropDown.SetOptions(messageNames, func(name string, index int) {
+		fmt.Fprintln(logs, "Selected "+name)
+		activeMessage = messages[index]
+	}).
+		SetLabel("Select message: ")
 
 	flex := tview.NewFlex().
 		AddItem(connectionsDropDown, 0, 1, true).
+		AddItem(messagesDropDown, 0, 2, false).
 		AddItem(logs, 0, 3, false)
 
 	if err := app.SetRoot(flex, true).Run(); err != nil {
@@ -49,15 +107,6 @@ func main() {
 	}
 
 	return
-	activeConnection = connections[0]
-
-	messages, err := loadMessages()
-	if err != nil {
-		fmt.Println("Can't load messages")
-		fmt.Println(err.Error())
-		return
-	}
-	activeMessage = messages[0]
 
 	fmt.Println("Connecting to '" + activeConnection.Name + "'")
 	client := GetClient(activeConnection)
