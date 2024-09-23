2426ba8dbcc2b939cec6f40db8d92054651410e1
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-09-23 22:24:15 +0200

    Remove log

--- app/persistence.go
+++ app/persistence.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"os"
 )
@@ -52,7 +51,6 @@ func loadMessages() ([]busMessage, error) {
         bytes = []byte("[]")
     }
     
-    fmt.Println("Bytes: ", len(bytes))
 	err = json.Unmarshal(bytes, &messages)
 	if err != nil {
 		return nil, err
