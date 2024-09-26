61f7aeef5a6bf03190ef629fe1664c8882418110
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-09-26 22:26:46 +0200

    Add right panel

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -11,6 +11,8 @@ type UI struct {
 	Connections  *tview.List
 	Messages     *tview.List
 	Destinations *tview.List
+	Content         *tview.TextView
+	Logs         *tview.TextView
 }
 
 func NewUI() *UI {
@@ -21,20 +23,29 @@ func NewUI() *UI {
 	ui.Connections = tview.NewList().ShowSecondaryText(false)
 	ui.Messages = tview.NewList().ShowSecondaryText(false)
 	ui.Destinations = tview.NewList().ShowSecondaryText(false)
+    ui.Content = tview.NewTextView()
+    ui.Logs = tview.NewTextView()
 
 	// Configure appearence
 	ui.Connections.SetTitle(" Connections: ").SetBorder(true)
 	ui.Messages.SetTitle(" Messages: ").SetBorder(true)
 	ui.Destinations.SetTitle(" Destinations: ").SetBorder(true)
+    ui.Content.SetTitle(" Content: ").SetBorder(true)
+    ui.Logs.SetTitle(" Logs: ").SetBorder(true)
 
 	//Set layouts
-	navigate := tview.NewFlex().SetDirection(tview.FlexRow).
+	left := tview.NewFlex().SetDirection(tview.FlexRow).
 		AddItem(ui.Connections, 0, 1, true).
 		AddItem(ui.Messages, 0, 1, false).
 		AddItem(ui.Destinations, 0, 1, false)
 
+    right := tview.NewFlex().SetDirection(tview.FlexRow).
+        AddItem(ui.Content, 0, 3, false).
+        AddItem(ui.Logs, 0, 1, false)
+
 	ui.Flex = tview.NewFlex().
-		AddItem(navigate, 0, 1, false)
+		AddItem(left, 0, 1, false).
+        AddItem(right, 0, 3, false)
 
 	return &ui
 }
