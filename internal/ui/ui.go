c2ed02e106418730c957c42a1c20318f79a15a69
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-09-26 17:37:06 +0200

    Add destinations

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -6,10 +6,11 @@ import "github.com/rivo/tview"
 type UI struct {
 
 	// View components.
-	App         *tview.Application
-	Grid        *tview.Grid
-	Connections *tview.List
-	Messages    *tview.List
+	App          *tview.Application
+	Grid         *tview.Grid
+	Connections  *tview.List
+	Messages     *tview.List
+	Destinations *tview.List
 }
 
 func NewUI() *UI {
@@ -19,15 +20,18 @@ func NewUI() *UI {
 	ui.App = tview.NewApplication()
 	ui.Connections = tview.NewList().ShowSecondaryText(false)
 	ui.Messages = tview.NewList().ShowSecondaryText(false)
+    ui.Destinations = tview.NewList().ShowSecondaryText(false)
 
 	// Configure appearence
 	ui.Connections.SetTitle("Connections: ").SetBorder(true)
 	ui.Messages.SetTitle("Messages: ").SetBorder(true)
+    ui.Destinations.SetTitle("Destinations: ").SetBorder(true)
 
 	//Set layouts
 	navigate := tview.NewGrid().SetRows(0, 0, 0).
 		AddItem(ui.Connections, 0, 0, 1, 1, 0, 0, true).
-		AddItem(ui.Messages, 1, 0, 1, 1, 0, 0, false)
+		AddItem(ui.Messages, 1, 0, 1, 1, 0, 0, false).
+        AddItem(ui.Destinations, 2, 0, 1, 1, 0, 0, false)
 
 	ui.Grid = tview.NewGrid().
 		SetRows(0, 2).
