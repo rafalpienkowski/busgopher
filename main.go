84ba70425f3bd4373732a0b8829e20804c550d06
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-09-27 14:53:59 +0200

    Clean main

--- main.go
+++ main.go
@@ -4,62 +4,25 @@ import (
 	"fmt"
 	"os"
 
-    "github.com/rafalpienkowski/busgopher/internal/ui"
-    "github.com/rafalpienkowski/busgopher/internal/config"
-    "github.com/rafalpienkowski/busgopher/internal/controller"
+	"github.com/rafalpienkowski/busgopher/internal/config"
+	"github.com/rafalpienkowski/busgopher/internal/controller"
+	"github.com/rafalpienkowski/busgopher/internal/ui"
 )
 
 func main() {
 
-    config := config.LoadConfig()
-    controller, err := controller.NewController(config)
+	config := config.LoadConfig()
+	controller, err := controller.NewController(config)
 	if err != nil {
 		fmt.Printf("Failed to start: %v\n", err)
 		os.Exit(1)
 	}
 
-    ui := ui.NewUI(controller)
-    ui.LoadData()
-    err = ui.Start()
+	ui := ui.NewUI(controller)
+	ui.LoadData()
+	err = ui.Start()
 	if err != nil {
 		fmt.Printf("failed to start: %v\n", err)
 		os.Exit(1)
 	}
-
-    /*
-	sendButton := tview.NewButton("Send")
-	sendButton.SetBorder(true)
-	closeButton := tview.NewButton("Exit").SetSelectedFunc(func() {
-		app.Stop()
-	})
-	closeButton.SetBorder(true)
-
-	inputs := []tview.Primitive{
-		connectionsSelection,
-		messagesSelection,
-		sendButton,
-		closeButton,
-	}
-
-
-	for _, conn := range connections {
-		connectionsSelection.AddItem(conn.Name, conn.Namespace, 'a', func() {
-			fmt.Fprintln(logs, "Selected "+conn.Name)
-			activeConnection = conn
-		})
-	}
-
-	for _, msg := range messages {
-		messagesSelection.AddItem(msg.Name, msg.Subject, 'a', func() {
-			fmt.Fprintln(logs, "Selected "+msg.Name)
-			activeMessage = msg
-			fmt.Fprintln(messagePreview, activeMessage.Body)
-		})
-	}
-
-	fmt.Println("Connecting to '" + activeConnection.Name + "'")
-	client := GetClient(activeConnection)
-
-	SendMessage(activeConnection.Destination, activeMessage, client)
-    */
 }
