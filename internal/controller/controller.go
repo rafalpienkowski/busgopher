fa6b093fb610f6546a328ec15adfb761fa7c6666
Rafał Pieńkowski <rafal.pienkowski@wp.pl>
2024-10-03 20:46:57 +0200

    Add cmd arguments support

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/rafalpienkowski/busgopher/internal/asb"
 	"github.com/rafalpienkowski/busgopher/internal/config"
 )
@@ -9,8 +12,9 @@ type Controller struct {
 	Connections []asb.Connection
 	Messages    []asb.Message
 
-	SelectedConnection asb.Connection
-	selectedMessage    asb.Message
+	SelectedConnection  asb.Connection
+	selectedMessage     asb.Message
+	selectedDestination string
 }
 
 func NewController(config *config.Config) (*Controller, error) {
@@ -20,6 +24,36 @@ func NewController(config *config.Config) (*Controller, error) {
 	controller.Messages = *config.Messages
 
 	return &controller, nil
+}
+
+func (controller *Controller) SelectConnectionByName(name string) error {
+	for _, conn := range controller.Connections {
+		if strings.EqualFold(conn.Name, name) {
+			controller.SelectedConnection = conn
+			return nil
+		}
+	}
+	return errors.New("Can't find connection with name: " + name)
+}
+
+func (controller *Controller) SelectDestinationByName(name string) error {
+	for _, dest := range controller.SelectedConnection.Entities {
+		if strings.EqualFold(dest, name) {
+			controller.selectedDestination = dest
+			return nil
+		}
+	}
+	return errors.New("Can't find destination with name: " + name)
+}
+
+func (controller *Controller) SelectMessageByName(name string) error {
+	for _, msg := range controller.Messages {
+		if strings.EqualFold(msg.Name, name) {
+			controller.selectedMessage = msg
+			return nil
+		}
+	}
+	return errors.New("Can't find message with name: " + name)
 }
 
 func (controller *Controller) SelectMessage(message asb.Message) {
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -11,18 +12,50 @@ import (
 
 func main() {
 
+	// Define flags
+	connection := flag.String("conn", "", "Saved connection name")
+	destination := flag.String("dest", "", "Destination")
+	message := flag.String("msg", "", "Message")
+
+	flag.Parse()
+
 	config := config.LoadConfig()
 	controller, err := controller.NewController(config)
 	if err != nil {
-		fmt.Printf("Failed to start: %v\n", err)
+		fmt.Printf("Failed to start controller: %v\n", err)
 		os.Exit(1)
 	}
 
-	ui := ui.NewUI(controller)
-	ui.LoadData()
-	err = ui.Start()
-	if err != nil {
-		fmt.Printf("failed to start: %v\n", err)
-		os.Exit(1)
+	// Check if used with params
+	if len(*connection) > 0 && len(*destination) > 0 && len(*message) > 0 {
+		fmt.Printf(
+			"Started headless mode with connection: %v, destination: %v, message: %v\n",
+			*connection,
+			*destination,
+			*message,
+		)
+        err := controller.SelectConnectionByName(*connection)
+        if err != nil {
+            fmt.Printf("%v\n", err.Error())
+            os.Exit(1)
+        }
+        err = controller.SelectDestinationByName(*destination)
+        if err != nil {
+            fmt.Printf("%v\n", err.Error())
+            os.Exit(1)
+        }
+        err = controller.SelectMessageByName(*message)
+        if err != nil {
+            fmt.Printf("%v\n", err.Error())
+            os.Exit(1)
+        }
+	} else {
+		ui := ui.NewUI(controller)
+		ui.LoadData()
+		err = ui.Start()
+		if err != nil {
+			fmt.Printf("Failed to start UI: %v\n", err)
+			os.Exit(1)
+		}
 	}
 }
